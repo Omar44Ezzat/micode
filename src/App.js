8f074053a4970c959a945875c7f4572b6c6020c2
Omar <omar47_ezzat@hotmail.comm>
2021-05-26 21:59:49 +0800

    addingVideo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ReactPlayer from 'react-player'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <ReactPlayer url= 'https://www.youtube.com/watch?v=R2pIutTspQA'/>
+        <img src="https://m.media-amazon.com/images/I/71+Q8W8ixtL._SS500_.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to MiCode, we are here to learn coding.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=R2pIutTspQA"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to code
         </a>
       </header>
     </div>
